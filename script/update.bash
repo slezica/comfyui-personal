4b410c12e35c5787baecfefa0e90850f92a98542
Santiago Lezica <salezica@gmail.com>
2025-10-01 02:19:34 -0300

    Update update script :D

--- script/update.bash
+++ script/update.bash
@@ -2,17 +2,10 @@ set -e
 
 ROOT=$(cd $(dirname "${BASH_SOURCE[0]}")/.. && pwd)
 
-echo "> Pulling from master"
-git checkout master
-git pull
+# Enter virtualenv:
+source venv/bin/activate
 
-echo "> Rebasing personal"
-git checkout personal
-git rebase master
-
-echo "> Updating base requirements"
-pip install -r requirements.txt
-
+# Upgrade plugins:
 while IFS= read -r dir; do
   cd "$ROOT/$dir"
 
@@ -27,7 +20,15 @@ done < <(ls -d custom_nodes/*/ | grep -v __)
 echo "> Updating independent requirements"
 pip install --upgrade onnxruntime insightface
 
+# Update core:
+echo "> Pulling ComfyUI"
+git checkout master
+git pull
+
+echo "> Updating ComfyUI requirements"
+pip install -r requirements.txt
+
 # echo "> Force-installing specific dependency versions"
 # pip install torch==2.3.1 torchaudio==2.3.1 torchvision==0.18.1 numpy==1.23.1
-
+#
 echo "> Done"
